663826f1fa34d76c2cdf97f3b7d82d1180cc93e0
Hieu Le <hieu.le@student.ufv.ca>
2021-04-05 16:42:37 -0700

    Updated question mockup data

--- controllers/questions.js
+++ controllers/questions.js
@@ -7,25 +7,37 @@ const questions = [
       'I am starting to study web development. Can someone give me a hint?',
     createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
     updatedAt: null,
-    userId: 1,
+    postedBy: {
+      userId: 1,
+      name: 'John Doe',
+    },
     comments: [
       {
-        id: 1,
-        commentUserId: 2,
+        commentId: 1,
+        postedBy: {
+          commentUserId: 2,
+          name: 'Hieu Le',
+        },
         content: 'MERN',
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: null,
       },
       {
-        id: 2,
-        commentUserId: 3,
+        commentId: 2,
+        postedBy: {
+          commentUserId: 3,
+          name: 'Gurjit Singh',
+        },
         content: 'MEAN',
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: null,
       },
       {
-        id: 3,
-        commentUserId: 3,
+        commentId: 3,
+        postedBy: {
+          commentUserId: 3,
+          name: 'Gurjit Singh',
+        },
         content: 'and PHP I think',
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: null,
@@ -34,25 +46,34 @@ const questions = [
   },
 
   {
-    id: 2,
+    commentId: 2,
     title: 'When will go back to UFV soon?',
     content:
       'It has been a while we did not go to the campus. I am just wondering when we go back to school?',
     createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
     updatedAt: null,
-    userId: 2,
+    postedBy: {
+      userId: 2,
+      name: 'Hieu Le',
+    },
     comments: [
       {
-        id: 4,
-        commentUserId: 1,
+        commentId: 4,
+        postedBy: {
+          commentUserId: 1,
+          name: 'John Doe',
+        },
         content: "I don't know",
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: null,
       },
 
       {
-        id: 5,
-        commentUserId: 2,
+        commentId: 5,
+        postedBy: {
+          commentUserId: 2,
+          name: 'Hieu Le',
+        },
         content: 'UFV announces that they will welcome us back in this Fall',
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
         updatedAt: null,
@@ -66,11 +87,17 @@ const questions = [
     content: 'As title, anyone can give me an advice, please?',
     createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
     updatedAt: null,
-    userId: 1,
+    postedBy: {
+      userId: 1,
+      name: 'John Doe',
+    },
     comments: [
       {
         id: 6,
-        commentUserId: 2,
+        postedBy: {
+          commentUserId: 2,
+          name: 'Hieu Le',
+        },
         content:
           'Every framework has its pro and const. Take a look here: https://www.sitepoint.com/react-vs-angular/',
         createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
