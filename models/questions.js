c5c5d49d1ef1f75a19b7bea82b3d82511ad9ce2e
Hieu Le <hieu.le@student.ufv.ca>
2021-04-05 21:36:28 -0700

    Removed 2 unsed classes in question model

--- models/questions.js
+++ models/questions.js
@@ -1,48 +1,31 @@
 const Database = require('../utils/db_query');
 
-// This class is for creating the model before CRUD operation
-class QuestionModel {
-  constructor(questionId, title, content) {
-    this.questionId = questionId;
-    this.title = title;
-    this.content = content;
-  }
-}
-
-// This class is for API JSON return
-class QuestionInfo extends QuestionModel {
-  constructor(questionId, title, content, createdAt, updatedAt, postedBy) {
-    super(questionId, title, content);
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.postedBy = postedBy;
-  }
-}
-
 // This class is used to perform the CRUD operations
 class Question {
   constructor() {}
   static async findAll() {
     const db = new Database();
     const query = `
-    SELECT q.questionId, q.title, q.content, u.userId, u.username, q.createdAt, q.updatedAt
+    SELECT q.questionId, q.title, q.content, u.userId, u.username, u.name, q.createdAt, q.updatedAt
     FROM questions q, users u
     WHERE q.questionId = u.userId`;
 
     const questions = await db.queryDatabase(query, []);
     let questionInfos = [];
     questions.forEach((question) => {
-      const questionInfo = new QuestionInfo(
-        question.questionId,
-        question.title,
-        question.content,
-        question.createdAt,
-        question.updatedAt,
-        {
+      const questionInfo = {
+        questionId: question.questionId,
+        title: question.title,
+        content: question.content,
+        createdAt: question.createdAt,
+        updatedAt: question.updatedAt,
+        postedBy: {
           userId: question.userId,
           username: question.username,
-        }
-      );
+          name: question.name,
+        },
+      };
+
       questionInfos.push(questionInfo);
     });
 
