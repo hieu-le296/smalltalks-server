9457304df6c214906bd178efbb7e92c0d710be94
Hieu Le <hieu.le@student.ufv.ca>
2021-04-06 12:47:24 -0700

    Restructred question Model and Controller

--- models/comments.js
+++ models/comments.js
@@ -1,66 +1,66 @@
 const Database = require('../utils/db_query');
 
 class Comment {
+  constructor(comment) {
+    //Object destructing to access fields of 'comment'
+    const {
+      commentId,
+      commentUserId,
+      name,
+      content,
+      createdAt,
+      updatedAt,
+    } = comment;
 
-    constructor(comment) {
+    //JSON Object structure for comment
+    this.data = {
+      commentId,
+      postedBy: {
+        commentUserId,
+        name,
+      },
+      content,
+      createdAt,
+      updatedAt,
+    };
+  }
 
-        //Object destructing to access fields of 'comment' 
-        const { commentId, commentUserId, name, content, createdAt, updatedAt } = comment;
-
-        //JSON Object structure for comment
-        this.data = {
-            commentId,
-            postedBy: {
-                commentUserId,
-                name
-            },
-            content,
-            createdAt,
-            updatedAt
-        }
-
-    }
-
-    /**
-     * Method to find all comments linked to a question having unique ID
-     *
-     */
-    static async findAll(questionId) {
-        const db = new Database();
-        const query =
-            `SELECT c.commentId, c.commentUserId, u.name, c.content, c.createdAt, c.updatedAt FROM comments c 
+  /**
+   * Method to find all comments linked to a question having unique ID
+   *
+   */
+  static async findAll(questionId) {
+    const db = new Database();
+    const query = `SELECT c.commentId, c.commentUserId, u.name, c.content, c.createdAt, c.updatedAt FROM comments c 
                 INNER JOIN users u ON 
                 c.commentUserId = u.userId 
                 WHERE c.questionId = ?;`;
 
-        const comments = await db.queryDatabase(query, [questionId]);
+    const comments = await db.queryDatabase(query, [questionId]);
 
-        const commentsData = [];
+    const commentsData = [];
 
-        //Format data for each comment 
-        comments.forEach(comment => commentsData.push(new Comment(comment).data));
+    //Format data for each comment
+    comments.forEach((comment) => commentsData.push(new Comment(comment).data));
 
-        return commentsData;
+    return commentsData;
+  }
 
-    }
-
-    /**
-     * Method to find one comment by id
-     */
-    static async findOne(id){
-        const db = new Database();
-        const query =
-            `SELECT c.commentId, c.commentUserId, u.name, c.content, c.createdAt, c.updatedAt FROM comments c 
+  /**
+   * Method to find one comment by id
+   */
+  static async findOne(id) {
+    const db = new Database();
+    const query = `SELECT c.commentId, c.commentUserId, u.name, c.content, c.createdAt, c.updatedAt FROM comments c 
             INNER JOIN users u ON 
             c.commentUserId = u.userId 
             WHERE c.commentId = ?`;
 
-        const comment = await db.queryDatabase(query, [id]);
+    const comment = await db.queryDatabase(query, [id]);
 
-        //Response from database is returned as array - so get the first element/row of array and format it in JSON
-        return new Comment(comment[0]).data;
-
-    }
+    //Response from database is returned as array - so get the first element/row of array and format it in JSON
+    return new Comment(comment[0]).data;
+  }
 }
 
 module.exports = Comment;
