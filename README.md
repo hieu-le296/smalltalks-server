7325329455dbf4ef1a713e03ea888e719cec43d7
Hieu Le <hieu.le@student.ufv.ca>
2021-04-06 18:47:55 -0700

    Added mockup data and seeder to import and delete data

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # datacomputation-server
 
 ## Allow ? and ' in the insert/update
+
+## Importing data
+
+In the terminal, type
+
+`node seeder -i`
+
+## Delete all data
+
+In the terminal, type
+
+`node seeder -d`
---
+++ seeder.js
@@ -0,0 +1,124 @@
+const fs = require('fs');
+const mysql = require('mysql');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env configurations
+dotenv.config({ path: './config/config.env' });
+
+const connectPool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
+
+function queryDatabase(query, values) {
+  return new Promise((resolve, reject) => {
+    connectPool.getConnection((err, con) => {
+      try {
+        con.query(query, values, (error, results) => {
+          try {
+            resolve(results);
+            con.release();
+          } catch (error1) {
+            reject(error);
+          }
+        });
+      } catch (error2) {
+        reject(err);
+      }
+    });
+  });
+}
+
+// Read JSON files
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
+const questions = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/questions.json`, 'utf-8')
+);
+
+const comments = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/comments.json`, 'utf-8')
+);
+
+async function createUsers(usersObj) {
+  const query = `INSERT INTO users(userId, name, username, email, passwd, role) VALUES ?`;
+
+  const userArr = [];
+
+  for (let i = 0; i < usersObj.length; i++) {
+    let user = [
+      usersObj[i].userId,
+      usersObj[i].name,
+      usersObj[i].username,
+      usersObj[i].email,
+      usersObj[i].password,
+      usersObj[i].role,
+    ];
+    userArr.push(user);
+  }
+  await queryDatabase(query, [userArr]);
+}
+
+async function createQuestions(questionsObj) {
+  const query = `INSERT INTO questions(questionId, userId, title, content) VALUES ?`;
+  const questionArr = [];
+  for (let i = 0; i < questionsObj.length; i++) {
+    let question = [
+      questionsObj[i].questionId,
+      questionsObj[i].userId,
+      questionsObj[i].title,
+      questionsObj[i].content,
+    ];
+    questionArr.push(question);
+  }
+  await queryDatabase(query, [questionArr]);
+}
+
+async function createComments(commentsObj) {
+  const query = `INSERT INTO comments(commentId, questionId, commentUserId, content) VALUES ?;`;
+  const commentArr = [];
+  for (let i = 0; i < commentsObj.length; i++) {
+    let comment = [
+      commentsObj[i].commentId,
+      commentsObj[i].questionId,
+      commentsObj[i].commentUserId,
+      commentsObj[i].content,
+    ];
+    commentArr.push(comment);
+  }
+  await queryDatabase(query, [commentArr]);
+}
+
+// Import into DB
+const importData = async () => {
+  try {
+    await createUsers(users);
+    await createQuestions(questions);
+    await createComments(comments);
+    console.log('Please Wait! Importing Data...'.green.inverse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Delete all columns and tables from DB
+const deleteData = async () => {
+  try {
+    await queryDatabase(`DELETE FROM users`, []);
+    console.log('Please Wait! Deleting Data...'.red.inverse);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
